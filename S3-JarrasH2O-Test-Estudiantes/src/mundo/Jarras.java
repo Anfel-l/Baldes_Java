464c552363a8b22746ec5c64f2d34d2f58bc47ea
Felipe <anfellr11@mail.com>
2022-09-09 12:40:24 -0500

    Some changes to the methos on ´Jarras.java´ class were made

--- S3-JarrasH2O-Test-Estudiantes/src/mundo/Jarras.java
+++ S3-JarrasH2O-Test-Estudiantes/src/mundo/Jarras.java
@@ -18,52 +18,64 @@ public class Jarras
     public void setJarra6(int jarra6) { this.jarra6 = jarra6; }
     public void setJarra8(int jarra8) { this.jarra8 = jarra8; }
 
-    public void llenar6()
-    {
-         if (jarra6<=6) {
-            jarra6 =6;
+    public void llenar6() {
+    
+        if (jarra6 < 6){
+            jarra6 = 6;
         }
     }    
-    public void llenar8()
-    {
-         if (jarra8<=8) {
-            jarra8 =8;
+    
+    public void llenar8() {
+    
+        if (jarra8 < 8){
+            jarra8 = 8;
         }
     }
     
-    public void descargar6()
-    {
-       if (jarra6>0) {
-            jarra6 =0;
+    public void descargar6() {
+    
+        if (jarra6 > 0){
+            jarra6 = 0;
         }
     }
-    public void descargar8()
-    {
-          if (jarra8>0) {
-            jarra8 =0;
+    public void descargar8() {
+        
+        if (jarra8 > 0){
+            jarra8 = 0;
         }
+
     }
  
-    public void llenar86()
-    {
-         if (jarra8<8 && jarra6>0) {
-             int aux = jarra6 + jarra8;
-             if (aux<=8)
-             {
-                 jarra8 = jarra8+jarra6;
-             }
+    public void llenar86() {
+    
+        if (jarra6 + jarra8 >= 8){
+            jarra6 = jarra6 - (8 - jarra8);
+            jarra8 = 8;
         }
     } 
-    public void llenar68()
-    {
+    
+    public void llenar68() {
         
+        if (jarra6 + jarra8 >= 6){
+            jarra8 = jarra8 - (6 - jarra6);
+            jarra6 = 6; 
+        }
     } 
 
-    public void vaciar86()
-    {
+    public void vaciar86() {
+        
+        if (jarra6 + jarra8 <= 6){
+            jarra6 = jarra6 + jarra8;
+            jarra8 = 0;
+        }
     } 
-    public void vaciar68()
-    {
+    
+    public void vaciar68() {
+        
+        if (jarra6 + jarra8 <= 8){
+            jarra8 = jarra8 + jarra6;
+            jarra6 = 0; 
+        }
     }
     
  
